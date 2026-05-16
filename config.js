baf6f15f30ca28043d6a6bdd35ec08d778a2b48a
mohanadehab7-sudo <mohanadehab7@gmail.com>
2026-05-16 18:39:09 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// App Configuration
+window.APP_CONFIG = {
+    SB_URL: 'https://bindidnosaflauksvfvb.supabase.co',
+    SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpbmRpZG5vc2FmbGF1a3N2ZnZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NjEwNTIsImV4cCI6MjA5NDQzNzA1Mn0.IO9hK7_rTdRUiAVp7jk1lWDziZBZYHYXw3CRzCd7mK8',
+    GH_REPO: 'mohanadehab7-sudo/crm'
+};
